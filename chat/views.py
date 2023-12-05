a8348222e1e0586263752c08cb4babd277b3dfad
4rkhamknight01 <devanshdasindia@outlook.com>
2023-12-05 20:46:42 +0530

    new changes

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Room, Message
 from django.http import HttpResponse, JsonResponse
+from cryptography.fernet import Fernet
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+def encrypt(msg):
+    return cipher_suite.encrypt(msg.encode())
 
 def room(request, room):
     username = request.GET.get('username')
@@ -31,15 +37,22 @@ def send(request):
     message = request.POST['message']
     username = request.POST['username']
     room_id = request.POST['room_id']
+    encrypted_message = encrypt(message)
+    print(encrypted_message)
      
     new_message = Message.objects.create(value=message, user=username, room=room_id)
     new_message.save()
     return HttpResponse('Message sent successfully')
 
+
 def getMessages(request, room):
     room_details = Room.objects.get(name=room)
     
     messages = Message.objects.filter(room=room_details.id)
+    # print(list(messages.values()))
+    arr = list(messages.values())
+    # print(encrypt(arr[0]))
+    # print(arr)
     return JsonResponse({"messages":list(messages.values())})
           
     
